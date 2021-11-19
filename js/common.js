285f246e69934a08c39fe7d12d543d9ae8be5158
Naveen <naveen@internetbusinesssolutionsindia.com>
2021-11-19 12:27:43 +0530

    first Commit

--- js/common.js
+++ js/common.js
@@ -199,31 +199,35 @@ setTimeout(function(){
 }, 10); 
 
 
-// ifreame load
-// var isInViewport = function (elem) {
-//   var bounding = elem.getBoundingClientRect();
-//   return (
-//       bounding.top >= 0 &&
-//       bounding.left >= 0 &&
-//       bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-//       bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
-//   );
-// };
-// window.addEventListener('scroll', function(event) {
-// var iframeLLVidContainer = document.querySelector(".iframe-lazy-load");
-// var iframeLLVids = document.querySelectorAll(".iframe-lazy-load");
-// var iframeVid = document.querySelector(".iframe-lazy-load iframe");
 
-// if(isInViewport(iframeLLVidContainer) && iframeVid == null) {
-//   iframeLLVids.forEach(function(videoDiv){ 
-//   var iframeVid = document.querySelector(".iframe-lazy-load iframe");
-//     var src = videoDiv.getAttribute("data-src");
-//     var front = '<iframe src=';
-//     var back = '" frameborder="0" allowfullscreen="allowfullscreen"></iframe>';
-//     videoDiv.innerHTML = (front + src + back);
-//   });
-// }
-// });
+
+
+
+// ifreame load
+var isInViewport = function (elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+      bounding.top >= 0 &&
+      bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+      bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+window.addEventListener('scroll', function(event) {
+var iframeLLVidContainer = document.querySelector(".iframe-lazy-load");
+var iframeLLVids = document.querySelectorAll(".iframe-lazy-load");
+var iframeVid = document.querySelector(".iframe-lazy-load iframe");
+
+if(isInViewport(iframeLLVidContainer) && iframeVid == null) {
+  iframeLLVids.forEach(function(videoDiv){ 
+  var iframeVid = document.querySelector(".iframe-lazy-load iframe");
+    var src = videoDiv.getAttribute("data-src");
+    var front = '<iframe src=';
+    var back = '" frameborder="0" allowfullscreen="allowfullscreen"></iframe>';
+    videoDiv.innerHTML = (front + src + back);
+  });
+}
+});
 
 
 var windowWidth = $(window).width();
@@ -232,7 +236,7 @@ $('.banner').mousemove(function(event){
   var moveY = (($(window).height() / 2) - event.pageY) * 0.1;
   $('.mousemove').css('margin-left', moveX + 'px');
   $('.mousemove').css('margin-top', moveY + 'px');
-}); 
+});  
 
 
 var $btns = $('.btn').click(function() {
